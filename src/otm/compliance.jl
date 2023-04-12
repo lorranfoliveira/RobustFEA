b380314a620f13d052e20265d4f02473aa581db0
Lorran Oliveira <lorranfoliveira@gmail.com>
2023-04-12 11:31:33 -0300

    Add diff_K to Compliance

--- src/otm/compliance.jl
+++ src/otm/compliance.jl
@@ -7,6 +7,7 @@ mutable struct Compliance
     structure::Structure
 end
 
+
 function H(compliance::Compliance)
     s = compliance.structure
     # Free forces with zeros
@@ -25,6 +26,28 @@ function H(compliance::Compliance)
     end
 
     return h
+end
+
+function diff_K(compliance::Compliance)
+    # TODO: Optimize this
+    # TODO: Implement virtual_dofs (degrees of freedom for constrained stiffness matrix)
+    # TODO: Change stiffness methods to K
+    n_dofs::Int64 = number_of_dofs(compliance.structure)
+    g::Vector{SparseMatrixCSC{Float64}} = []
+
+    for element in compliance.structure.elements
+        gi::SparseMatrixCSC{Float64} = spzeros(n_dofs, n_dofs)
+        dofs = dofs(element)
+
+        tmp_area = element.area
+        element.area = 1.0
+        gi[dofs, dofs] = stiffness_global(element)
+        element.area = tmp_area
+
+        push!(g, gi)
+    end
+
+    return g
 end
 
 function Z(compliance::Compliance)
