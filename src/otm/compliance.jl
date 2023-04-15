0dbb41e56c28d67ee78898ae5a9b49cc4de8b67a
Lorran Oliveira <lorranfoliveira@gmail.com>
2023-04-15 15:08:49 -0300

    Add compliance sensitivity

--- src/otm/compliance.jl
+++ src/otm/compliance.jl
@@ -5,6 +5,59 @@ using LinearAlgebra,
 
 mutable struct Compliance
     structure::Structure
+    p::Float64
+    eig_vals::Vector{Float64}
+    eig_vecs::Matrix{Float64}
+
+    function Compliance(structure::Structure, p::Float64=1.0)
+        new(structure, p, [], [])
+    end
+end
+
+
+function calculate_C_eigenvals_and_eigenvecs(compliance::Compliance)
+    c::Matrix{Float64} = C(compliance)
+    compliance.eig_vals, compliance.eig_vecs = eigen(c)
+end
+
+
+function diff_eigenvals(compliance::Compliance)
+    num_eigvals::Int64 = length(compliance.eig_vals)
+    num_design_vars::Int64 = length(compliance.structure.elements)
+    dC::Vector{Matrix{Float64}} = diff_C(compliance)
+    
+    g::Matrix{Float64} = zeros(num_eigvals, num_design_vars)
+
+    for i=1:num_eigvals
+        vecs::Vector{Float64} = compliance.eig_vecs[:, i]
+
+        for j=1:num_design_vars
+            g[i, j] = vecs' * dC[j] * vecs
+        end
+    end
+
+    return g
+end
+
+
+function diff_obj_smooth(compliance::Compliance)
+    calculate_C_eigenvals_and_eigenvecs(compliance)
+
+    num_eigvals::Int64 = length(compliance.eig_vals)
+    num_design_vars::Int64 = length(compliance.structure.elements)
+
+    grad::Vector{Float64}(undef, num_design_vars)
+    grad_eigvals::Matrix{Float64} = diff_eigenvals(compliance)
+
+    cons::Float64 = (norm(compliance.eig_vals, compliance.p) ^ (compliance.p - 1))
+
+    for i=1:num_design_vars
+        for j=1:num_eigvals
+            grad[i] += grad_eigvals[i, j] * (compliance.eig_vals[j] ^ (compliance.p - 1)) / cons
+        end
+    end
+
+    return grad
 end
 
 
@@ -23,38 +76,31 @@ function H(compliance::Compliance)
     return h
 end
 
-function diff_K(element::Element)
+function diff_K(element::Element)::SparseMatrixCSC{Float64}
     aux::Float64 = element.area
     element.area = 1.0
-    k = K(element)
+    k::SparseMatrixCSC{Float64} = spzeros(4, 4)
+    
+    free_dofs::Vector{Int64} = dofs(element, include_restricted=false, local_dofs=true)
+    k[free_dofs, free_dofs] = K(element)[free_dofs, free_dofs]
     element.area = aux
     
     return k
 end
 
-function diff_K(compliance::Compliance)::Vector{SparseMatrixCSC{Float64}}
-    # TODO: Optimize this
-    # TODO: Implement virtual_dofs (degrees of freedom for constrained stiffness matrix)
-    # TODO: Change stiffness methods to K0
-    g::Vector{SparseMatrixCSC{Float64}} = []
-
-    for element in compliance.structure.elements
-        gi = spzeros(4, 4)
-        dofs_el_loc = dofs(element, include_restricted=true, local_dofs=true)
-        gi[dofs_el_loc, dofs_el_loc] = diff_K(element)[dofs_el_loc, dofs_el_loc]
-
-        push!(g, gi)
-    end
-
-    return g
-end
+diff_K(compliance::Compliance)::Vector{SparseMatrixCSC{Float64}} = [diff_K(element) for element in compliance.structure.elements]
 
 function Z(compliance::Compliance)
-    return K(compliance.structure, include_restricted=true) \ H(compliance)
+    return K(compliance.structure) \ H(compliance)
 end
 
+function C(compliance::Compliance)::Matrix{Float64}
+    z::Matrix{Float64} = Z(compliance)
+    return z' * K(compliance.structure) * z
+end 
+
 function diff_C(compliance::Compliance)
-    g::Vector{Float64} = []
+    g::Vector{Matrix{Float64}} = []
     z::Matrix{Float64} = Z(compliance)
 
     for element in compliance.structure.elements
