4fdfc6d254b10f61b7de9aac487eca86590bc3dd
Lorran Oliveira <lorranfoliveira@gmail.com>
2023-09-11 21:08:45 -0300

    Revert "finish"
    This reverts commit b37c94c8bad7f1fb38313b7bd905c50408ab7a45, reversing
    changes made to 094992de14b3aae9dcaf905525bf7317dbe6325c.

--- main.jl
+++ main.jl
@@ -1,9 +1,5 @@
 include("src/otm/otm.jl")
 
 otm = generate_optimizer(ARGS[1])
-<<<<<<< HEAD
 #otm = generate_optimizer("examples/hook/case_2.json")
-=======
-# otm = generate_optimizer("examples/hook/case_2.json")
->>>>>>> 0db2847109cf6e6eb66aeacb843ecaf13e9cdc2c
 optimize!(otm)
--- run.py
+++ run.py
@@ -3,11 +3,7 @@ from data_handler import Modeller, SaveData, Optimizer, ComplianceMu, Material
 
 # ================================ Defining case ================================
 case = 4
-<<<<<<< HEAD
 only_read = False
-=======
-only_read = True
->>>>>>> 0db2847109cf6e6eb66aeacb843ecaf13e9cdc2c
 case_ref = 1  # DO NOT CHANGE THIS
 
 # ================================ Create json file ================================
@@ -76,9 +72,5 @@ modeller = Modeller.read(f'examples/hook/case_{case}.json')
 #                                   plot_supports=False)
 #
 
-<<<<<<< HEAD
 # modeller.plot_dv_analysis(Modeller.read(f'examples/hook/case_{case_ref}.json'), width=1.1)
-=======
-modeller.plot_dv_analysis(Modeller.read(f'examples/hook/case_{case_ref}.json'), width=1.1)
->>>>>>> 0db2847109cf6e6eb66aeacb843ecaf13e9cdc2c
 # modeller.plot_dv_one_case(width=1.1)
